caeb1ef009fd6c64d71b408981bc2bd7dbcbcf15
JoseASantoss <j.santos.sanchez@gmail.com>
2021-02-08 23:21:21 +0100

    On branch jass Changes to be committed: 	modified:   src/Principal.java 	modified:   src/com/itt/calculadora/Suma.java
    Actualización de la clase suma quitando todos los acentos para hacer
    el código más legible.
    Mostrar mensaje por pangalla cuando el número pasado sea negativo
    Actualizar en la clase Principal el apartado Suma para que siga el
    modelo de presentación de Álvaro.

--- src/com/itt/calculadora/Suma.java
+++ src/com/itt/calculadora/Suma.java
@@ -2,13 +2,13 @@ package com.itt.calculadora;
 
 
 /**
- * <p>Clase est&aacutetica que realiza las operaciones b&aacutesicas de suma en una calculadora: <br>
- * &nbsp&nbsp- suma de dos n&uacutemeros reales<br>
- * &nbsp&nbsp- suma de dos n&uacutemeros enteros<br>
- * &nbsp&nbsp- suma de tres n&uacutemeros reales<br>
+ * <p>Clase estatica que realiza las operaciones basicas de suma en una calculadora: <br>
+ * &nbsp&nbsp- suma de dos numeros reales<br>
+ * &nbsp&nbsp- suma de dos numeros enteros<br>
+ * &nbsp&nbsp- suma de tres numeros reales<br>
  * &nbsp&nbsp- suma con memoria<br>
- * No est&aacute permitido usar n&uacutemeros negativos en la suma. Su uso inducir&aacute a un error
- * en la aplicaci&oacuten</p>
+ * No esta permitido usar numeros negativos en la suma. Su uso inducira a un error
+ * en la aplicacion</p>
  * <hr/>
  * <h3>Metodos que implementa:</h3>
  * <p>
@@ -23,35 +23,35 @@ package com.itt.calculadora;
  * 			<li><h3><em>{@link #sumReales(double, double)}:</em></h3>
  * 				<ul>
  * 					<li>
- * 						M&eacutetodo para la suma de dos n&uacutemeros reales.
+ * 						Metodo para la suma de dos numeros reales.
  * 					</li>
  * 				</ul>
  * 			</li>
  * 			<li><h3><em>{@link #sumEntero(int, int)}:</em></h3>
  * 				<ul>
  * 					<li>
- * 						M&eacutetodo para la suma de dos n&uacutemeros enteros.
+ * 						Metodo para la suma de dos numeros enteros.
  * 					</li>
  * 				</ul>
  * 			</li>
  * 			<li><h3><em>{@link #sumTresReales(double, double, double)}:</em></h3>
  * 				<ul>
  * 					<li>
- * 						M&eacutetodo para la suma de tres n&uacutemeros reales.
+ * 						Metodo para la suma de tres numeros reales.
  * 					</li>
  * 				</ul>
  * 			</li>
  * 			<li><h3><em>{@link #sumAcumulado(double)}:</em></h3>
  * 				<ul>
  * 					<li>
- * 						M&eacutetodo para la suma con memoria.
+ * 						Metodo para la suma con memoria.
  * 					</li>
  * 				</ul>
  * 			</li>
  * 			<li><h3><em>{@link #getAcum()}:</em></h3>
  * 				<ul>
  * 					<li>
- * 						M&eacutetodo para poder mostar el resultado de la suma acumulada
+ * 						Metodo para poder mostar el resultado de la suma acumulada
  * 					</li>
  *	 			</ul>
  * 			</li>
@@ -60,16 +60,16 @@ package com.itt.calculadora;
  *<hr/>
  *<h2>Casos especiales:</h2>
  *<ol>
- *	<li><p>Utilizaci&oacuten de n&uacutemeros negativos no permitidos:</p>
- * 		Si uno de los n&uacutemeros pasados es negativo se lanzar&aacute un aviso de que no est&aacute
- * 		permitido su uso, no realizando la operaci&oacute.
+ *	<li><p>Utilizacion de numeros negativos no permitidos:</p>
+ * 		Si uno de los numeros pasados es negativo se lanzara un aviso de que no esta
+ * 		permitido su uso, no realizando la operacio.
  * 	</li>
- * 	<li><p>Usar caracteres en vez de n&uacutemeros:
- * 		Si en vez de n&uacutemeros como argumentos se quiere pasar caracteres, puede suceder:<br> 
+ * 	<li><p>Usar caracteres en vez de numeros:
+ * 		Si en vez de numeros como argumentos se quiere pasar caracteres, puede suceder:<br> 
  * &nbsp&nbsp&nbsp- Por linea de comandos: el tipo de variable definido ya impide que se pasen caracteres<br>
  * &nbsp&nbsp&nbsp dando un error en tiempo de compilación.
  * &nbsp&nbsp&nbsp- Por consola mediante la clase Scanner: Al implementar la clase Scanner se puede controlar<br>
- * &nbsp&nbsp&nbsp que el usuario no pase nig&uacuten car&aacutecter controlandolo por condionales o usando la <br>
+ * &nbsp&nbsp&nbsp que el usuario no pase nigun caracter controlandolo por condionales o usando la <br>
  * &nbsp&nbsp&nbsp InputMismatchException de la clase java.util.
  * 	</li>
  *</ol>
@@ -79,83 +79,89 @@ package com.itt.calculadora;
  * @author jass
  */
 public class Suma {
-	
+	/**
+	 * El atributo acum recoge el acumulado de la suma.
+	 * ES usado en el metodo {@link #sumAcumulado(double)}
+	 */
 	private static double acum = 0;
 	
 	/**
-	 * Constructor por defecto que no recibe ning&uacuten par&aacutemetro.
+	 * Constructor por defecto que no recibe ningun parametro.
 	 */
 	public Suma() {
 		
 	}
 	
 	/**
-	 * M&eacutetodo est&aacutetico que recibe dos n&uacutemeros reales como argumentos y devuelve la suma de ambos.
+	 * Metodo estatico que recibe dos numeros reales como argumentos y devuelve la suma de ambos.
 	 * @param num1
-	 * 		Sumando que recibe un n&uacutemero real
+	 * 		Sumando que recibe un numero real
 	 * @param num2
-	 * 		Sumando que recibe un n&uacutemero real
+	 * 		Sumando que recibe un numero real
 	 * @return
-	 * 		Un n&uacutemero real que es el resultado de la suma de num1 + num2.
+	 * 		Un numero real que es el resultado de la suma de num1 + num2.
 	 * 
-	 * Si uno de los valores es negativo no se realiar&aacute la operaci&oacuten, devolviendo 0.00;
+	 * Si uno de los valores es negativo no se realiara la operacion, devolviendo 0.00;
 	 */
 	public static double sumReales(double num1, double num2) {
 		if((num1<0 || num2 <0)) {
+			System.out.println("La calculadora solo admite numeros positivos.");
 			return 0.00;
 		}
 		return num1+num2;
 	}
 	
 	/**
-	 * M&eacutetodo est&aacutetico que recibe dos n&uacutemero enteros como argumentos y 
+	 * Metodo estatico que recibe dos numero enteros como argumentos y 
 	 * devuelve la suma de ambos.
 	 * @param num1
-	 * 		Sumando que recibe un n&uacutemero entero
+	 * 		Sumando que recibe un numero entero
 	 * @param num2
-	 * 		Sumando que recibe un n&uacutemero entero
+	 * 		Sumando que recibe un numero entero
 	 * @return
 	 * 		El resultado de la suma de num1 y num2.
 	 * 
-	 * Si uno de los valores es negativo no se realiar&aacute la operaci&oacuten, devolviendo 0;
+	 * Si uno de los valores es negativo no se realiara la operacion, devolviendo 0;
 	 */
 	public static int sumEntero(int num1, int num2) {
 		if(num1<0 || num2 <0) {
+			System.out.println("La calculadora solo admite numeros positivos.");
 			return 0;
 		}
 		return num1+num2;
 	}
 	
 	/**
-	 * M&eacutetodo est&aacutetico que recibe tres n&uacutemeros reales como argumentos y devuelve la suma de ambos.
+	 * Metodo estatico que recibe tres numeros reales como argumentos y devuelve la suma de ambos.
 	 * @param num1
-	 * 		Sumando que recibe un n&uacutemero real
+	 * 		Sumando que recibe un numero real
 	 * @param num2
-	 * 		Sumando que recibe un n&uacutemero real
+	 * 		Sumando que recibe un numero real
 	 * @param num3
-	 * 		Sumando que recibe un n&uacutemero real
+	 * 		Sumando que recibe un numero real
 	 * @return
-	 * 		Un n&uacutemero real que es el resultado de la suma de num1, num2 y num3
+	 * 		Un numero real que es el resultado de la suma de num1, num2 y num3
 	 * 
-	 * Si uno de los valores es negativo no se realiar&aacute la operaci&oacuten, devolviendo 0.00;
+	 * Si uno de los valores es negativo no se realiara la operacion, devolviendo 0.00;
 	 */
 	public static double sumTresReales(double num1, double num2, double num3) {
 		if(num1<0 || num2 <0 || num3<0) {
+			System.out.println("La calculadora solo admite numeros positivos.");
 			return 0.00;
 		}
 		return num1+num2+num3;
 	}
 	
 	/**
-	 * M&eacutetodo est&aacutetico que acumula el n&uacutemero que se vaya pasando 
-	 * como par&aacutemetro, con el fin de realizar una suma acumulada.<br>
+	 * Metodo estatico que acumula el numero que se vaya pasando 
+	 * como parametro, con el fin de realizar una suma acumulada.<br>
 	 * Para consultar el valor acumulado de la suma usamos:
 	 * {@link #getAcum()}
 	 * @param num
-	 * 		N&uacutemero de tipo real 
+	 * 		Numero de tipo real 
 	 * 
-	 * Si uno de los valores es negativo no se realiar&aacute la operaci&oacuten.<br>
-	 * Se mostrar&aacute un aviso de que el n&uacutemero no ha sido tenido en cuenta.
+	 * Si uno de los valores es negativo no se realiara la operacion.<br>
+	 * Se mostrara un aviso de que el numero no ha sido tenido en cuenta.
 	 */
 	
 	public static void sumAcumulado(double num) {
@@ -167,7 +173,7 @@ public class Suma {
 		}
 	}
 	/**
-	 * M&eacutetodo est&aacutetico que no recibe par&aacutemetros y devuelve el valor acumulado de la suma.
+	 * Metodo estatico que no recibe parametros y devuelve el valor acumulado de la suma.
 	 * @return
 	 * 		Devuelve el acumulado de la suma que se ha ido acumulando.
 	 * 		{@link #sumAcumulado(double)}
